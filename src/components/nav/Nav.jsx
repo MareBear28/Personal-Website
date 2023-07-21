dba5175c973493bb2f3738f8eb943b558a9fd390
MareBear28 <marielurbano28@gmail.com>
2023-07-20 17:02:21 -0700

    Created Nav Bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome, AiOutlineUser, AiOutlineFolderOpen, AiOutlineMail} from 'react-icons/ai'
 import {MdWorkOutline, MdOutlineDraw, MdPeopleOutline} from 'react-icons/md'
+import {useState} from 'react'
 
 const Nav = () => {
+  const[navActive, setNavActive] = useState('#')
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><MdWorkOutline/></a>
-      <a href="#commissions"><MdOutlineDraw/></a>
-      <a href="#portfolio"><AiOutlineFolderOpen/></a>
-      <a href="#reviews"><MdPeopleOutline/></a>
-      <a href="#contact"><AiOutlineMail/></a>
+      <a href="#" className={navActive === '#'? 'active': ''} onClick={() => setNavActive('#')}><AiOutlineHome/></a>
+
+      <a href="#about" className={navActive === '#about'? 'active': ''} onClick={() => setNavActive('#about')}><AiOutlineUser/></a>
+      
+      <a href="#experience" className={navActive === '#experience'? 'active': ''} onClick={() => setNavActive('#experience')}><MdWorkOutline/></a>
+
+      <a href="#commissions" className={navActive === '#commissions'? 'active': ''} onClick={() => setNavActive('#commissions')}><MdOutlineDraw/></a>
+
+      <a href="#portfolio" className={navActive === '#portfolio'? 'active': ''} onClick={() => setNavActive('#portfolio')}><AiOutlineFolderOpen/></a>
+
+      <a href="#reviews" className={navActive === '#reviews'? 'active': ''} onClick={() => setNavActive('#reviews')}><MdPeopleOutline/></a>
+      
+      <a href="#contact" className={navActive === '#contact'? 'active': ''} onClick={() => setNavActive('#contact')}><AiOutlineMail/></a>
     </nav>
   )
 }
