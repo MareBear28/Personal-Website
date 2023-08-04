38ba506a7e3861ac3804066f742285aac5bd575f
MareBear28 <marielurbano28@gmail.com>
2023-08-03 21:55:16 -0700

    Created Experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,118 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__languages">
+          <h3>Languages</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Kotlin</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
 
-export default Experience
+        <div className="experience__tools">
+          <h3>Tools</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>VS Code</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>IntelliJ</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>GitHub</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Figma</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Firebase</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillCheckCircleFill className="experience__details-icon"/>
+              <div>
+                <h4>Android Studio</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
